9f5ffeea2c03c79064e0a6efeba31b82f1dbb85b
jm5487 <jm5487@msstate.edu>
2025-04-14 19:48:59 -0500

    Add files via upload

---
+++ IRSENSOR_BUZZER.ino
@@ -0,0 +1,90 @@
+// Define the IR sensor pin
+const int irSensorPin = 2;
+
+// Define the buzzer pin
+const int buzzerPin = 3;
+
+void setup() {
+  // Set the IR sensor pin as an input
+  pinMode(irSensorPin, INPUT);
+
+  // Set the buzzer pin as an output
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  // Read the IR sensor value
+  int sensorValue = digitalRead(irSensorPin);
+
+  // If the sensor detects an object (or whatever state you want to trigger the buzzer)
+  if (sensorValue == LOW) { // Or LOW, depending on your sensor's output
+    // Turn on the buzzer
+    tone(buzzerPin, 1000); // Play a tone at 3000 Hz
+    delay(500); // Delay for 500 milliseconds
+    noTone(buzzerPin); // Stop the tone
+  }
+  delay(10);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const int IRSensorPin = 3;
+const int LEDPin = 10;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(IRSensorPin, INPUT);
+
+  pinMode(LEDPin, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = digitalRead(IRSensorPin); // Read the IR sensor output
+
+  if (sensorValue == LOW) {
+    digitalWrite(LEDPin, HIGH);
+    Serial.println("There is an obstacle!");
+  } 
+  
+  else {
+    digitalWrite(LEDPin, LOW);
+    Serial.println("There is no obstacle!");
+  }
+
+
+  delay(2);
+}
+*/
